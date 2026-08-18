402fe145a18c959db9f2c699bc7180333547291e
VigneshwariMurugan81 <vigneshwarimurugan81@gmail.com>
2026-08-18 11:27:03 +0530

    Fix Render port

--- app.py
+++ app.py
@@ -139,4 +139,7 @@ demo = gr.Interface(
 # 9. Launch application
 # -----------------------------------
 
-demo.launch()
+demo.launch(
+    server_name="0.0.0.0",
+    server_port=int(os.environ.get("PORT", 7860))
+)
